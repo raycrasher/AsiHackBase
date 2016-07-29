31bdb71c9c86d870210d60ff50db5f758f0e9114
Romelyn <romrod1010@gmail.com>
2016-07-29 22:46:46 +0800

    Updated Login.js

--- AsiHack.Base/Script/Login.js
+++ AsiHack.Base/Script/Login.js
@@ -1,19 +1,29 @@
 $(document).ready(function(){
 	$("#btnSubmit").click(function(){
-        urlToHandler = 'handler.ashx';
-		jsonData = '{ "username":"2010/01/01", "password": "hello" }';
+        urlToHandler = '/login';
+		var obj = new Object();
+		obj.username = $("#username").val();
+		obj.password = $("#password").val();
+		var jsonData = JSON.stringify(obj);
 			$.ajax({
                 url: urlToHandler,
                 data: jsonData,
                 dataType: 'json',
                 type: 'POST',
                 contentType: 'application/json',
-                success: function(data) {                        
-                  alert("Success");  
+                success: function(status) { 
+						var obj = jQuery.parseJSON(status);
+						if(obj.success == 1){
+							window.location = '/homepage'
+						}
+                        else if(obj.success == 0){
+							$("#loginStatus").attr('class', 'ui-state-error')  
+							.html('<strong>ERROR</strong>: Your details were incorrect.<br />');
+						}						
 				},
                 error: function(data, status, jqXHR) {                        
                     alert('There was an error.');
                 }
-            }); // end $.ajax
+            });
     }); 
 });
