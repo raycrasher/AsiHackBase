7b7c268a2003d2c72f3ed9c8c16fc65ec20c87bb
Arthur A. Almanzor Jr <arthuralmanzor@gmail.com>
2016-07-29 21:55:28 +0800

    Base for frontend

---
+++ AsiHack/assets/js/base/htmlLoader.js
@@ -0,0 +1,27 @@
+function loadHtml(elID, url){	
+	var xmlhttp;
+
+	if(window.XMLHttpRequest){
+		xmlhttp = new XMLHttpRequest();
+	}else{
+		xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+	}
+
+	xmlhttp.onreadystatechange = function(){
+		if(xmlhttp.readyState == 4){
+			if(xmlhttp.status == 200){
+				document.getElementById(elID).innerHTML = xmlhttp.responseText;
+			}else{
+				alert("error");
+			}
+		}
+	}
+
+	xmlhttp.open("GET", url, true);
+	xmlhttp.send(null);
+
+
+}
+
+loadHtml("sideNav", "views/sideNav.html");
+loadHtml("topNav", "views/topNav.html");
