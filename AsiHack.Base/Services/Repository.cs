06d5d32791379461dccedb64ad34f5c475c82f5f
David Garcia <cinquedia@gmail.com>
2016-07-30 14:26:18 +0800

    Add repo pattern

---
+++ AsiHack.Base/Services/Repository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AsiHack.Base.Models;
+using System.Data.Entity;
+
+namespace AsiHack.Base.Services
+{
+    public interface IRepository
+    {
+        IEnumerable<User> Users { get; }
+    }
+
+    public class Repository: DbContext, IRepository
+    {
+        IDbSet<User> Users;
+        IEnumerable<User> IRepository.Users => Users;
+
+        public Repository()
+        {
+            
+        }
+    }
+}
