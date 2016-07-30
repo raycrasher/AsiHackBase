06acd3edd00cf1c56765526afd4889df69576157
David Garcia <cinquedia@gmail.com>
2016-07-30 16:54:57 +0800

    Update from web

---
+++ Alliance.NihonggoFlash/Services/RetrieveDataFromOnline.cs
@@ -0,0 +1,42 @@
+﻿using Alliance.NihonggoFlash.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alliance.NihonggoFlash.Services
+{
+    public class RetrieveDataFromOnline
+    {
+        public void RetrieveData()
+        {
+            try
+            {
+                var request = WebRequest.Create(Properties.Settings.Default.FlashCardUpdateUrl);
+                request.Method = "GET";
+                var response = request.GetResponse();
+                using (var reader = new System.IO.StreamReader(response.GetResponseStream()))
+                {
+                    var db = SimpleIoc.Default.GetInstance<Repository>();
+                    var responseStr = reader.ReadToEnd();
+                    var cards = Newtonsoft.Json.JsonConvert.DeserializeObject<List<FlashCard>>(responseStr);
+
+                    if (cards.Count > 0)
+                    {
+                        db.Database.ExecuteSqlCommand("delete from Cards");
+                        foreach (var card in cards)
+                        {
+                            db.Cards.Add(card);
+                        }
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                // dunno what to do here yet                
+            }
+        }
+    }
+}
