2ed01e94f7cd9f72c5f4a781d160e3b38a243cfe
David Garcia <cinquedia@gmail.com>
2016-07-30 20:02:13 +0800

    compile

--- AsiHack.Base/Modules/Homepage.cs
+++ AsiHack.Base/Modules/Homepage.cs
@@ -25,25 +25,6 @@ namespace AsiHack.Base.Modules
             {
                 return View["SideNav"];
             };
-
-            Post["/SetData"] = _ =>
-            {
-                /*var repo = SimpleIoc.Default.GetInstance<Repository>();
-                List<FlashCardPostData> data;
-                using (var stream = new System.IO.StreamReader(Request.Body))
-                {
-                    var theString = System.Text.RegularExpressions.Regex.Unescape(stream.ReadToEnd());
-                    data = Newtonsoft.Json.JsonConvert.DeserializeObject<List<FlashCardPostData>>(theString);
-                }
-                if (data == null) return Response.AsJson(new Status(0, "Unable to parse input JSON"));
-                foreach (var d in data)
-                {
-                    repo.Cards.Add(d.CreateFlashCard());
-                }
-
-                repo.SaveChanges();*/
-                return Response.AsJson(new Status(1, "OK", $"Added {data.Count} items"));
-            };
         }
 
     }
